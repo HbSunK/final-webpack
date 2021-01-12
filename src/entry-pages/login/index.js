d28afe2d8b9299a9fa3d90d7c36caa388c04eb95
孙凯 <sunkai60@jd.com>
2021-01-12 17:10:52 +0800

    test

--- src/entry-pages/login/index.js
+++ src/entry-pages/login/index.js
@@ -1,1 +1,12 @@
 require('./public/css/index.scss')
+
+import Vue from 'vue';
+import vuex from 'vuex';
+import router from 'vue-router';
+import App from './index.vue'
+
+const myVue = new Vue({
+    render () {
+        return <App />
+    }
+}).$mount('#app');
---
+++ src/entry-pages/login/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>{{a}}------{{b}}<Button/></div>
+</template>
+
+<script>
+import { Button } from 'vant';
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            a: 1,
+            b: 2
+        }
+    },
+    created() {
+        console.log('vue data ======>')
+    },
+    components: {
+        [Button.name]: Button
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+    body {
+        div {
+            background: greenyellow no-repeat left top;
+            // background-image: url(./public/img/sprite.src-img1.png);
+            background-size: 10% 10%;
+            transform: translateX(30px)
+        }
+    }
+</style>
